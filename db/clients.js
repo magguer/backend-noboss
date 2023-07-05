83f3becdc936d8147e49bf3177d6bb858556441e
magguer <martintelaguer@gmail.com>
2023-07-05 17:40:10 -0300

    clients-db

--- db/clients.js
+++ db/clients.js
@@ -1,4 +1,5 @@
 const defaultClients = [
+    //Atic
     {
         name: "Dylan Blasi",
         type: "Casual",
@@ -10,6 +11,216 @@ const defaultClients = [
         bookings: [],
         bookings_quantity: 0
     },
+    {
+        name: "Raul Rodriguez",
+        type: "Casual",
+        email: "raul@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Agustin Garcia",
+        type: "Casual",
+        email: "agustin@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Susana Blasi",
+        type: "Casual",
+        email: "susana@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Manuel",
+        type: "Casual",
+        email: "manuel@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "MercadoLibre",
+        type: "Casual",
+        email: "mercadolibre@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Matías",
+        type: "Casual",
+        email: "matías1@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "María",
+        type: "Casual",
+        email: "maría684@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Helena Aguerre",
+        type: "Casual",
+        email: "helenita@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Carolina",
+        type: "Casual",
+        email: "caro@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Magdalena",
+        type: "Casual",
+        email: "maggie@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Lucas S.",
+        type: "Casual",
+        email: "lucass23@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Salomon",
+        type: "Casual",
+        email: "salomondrin@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Ezquiel Blasi",
+        type: "Casual",
+        email: "ezeqblasi@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Benito",
+        type: "Casual",
+        email: "badbunny@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "El de la esquina",
+        type: "Casual",
+        email: "eldelaesqui@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Maite Marus",
+        type: "Casual",
+        email: "maruss@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Maniquin",
+        type: "Casual",
+        email: "maniquin@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Juliet",
+        type: "Casual",
+        email: "julieta@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    {
+        name: "Marta Garcia",
+        type: "Casual",
+        email: "martagarcia@gmail.com",
+        phone: "092683955",
+        project: "atic",
+        orders: [],
+        orders_quantity: 0,
+        bookings: [],
+        bookings_quantity: 0
+    },
+    //CuartoExpreso
     {
         name: "Garabato",
         type: "Recurrente",
